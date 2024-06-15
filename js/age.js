abf9cf8c6595f8d9911eaf1d108378356c092d4a
mannishmajhi <158675603+mannishmajhi@users.noreply.github.com>
2024-06-15 16:01:09 +0545

    added age.js

---
+++ js/age.js
@@ -0,0 +1,33 @@
+const birth_year = 2000;
+const birth_month = 5;
+const birth_date = 24;
+
+const today = new Date();
+const current_year = today.getFullYear();
+const current_month = today.getMonth() + 1; //because stupid getMonth() starts from 0
+const current_day = today.getDate();
+
+let age_years = current_year - birth_year;
+let age_months = current_month - birth_month;
+let age_days = current_day - birth_date;
+
+if (age_days < 0) {
+  const previous_month = new Date(current_year, current_month - 1, 0);
+  age_days += previous_month.getDate();
+  age_months--;
+}
+
+if (age_months < 0) {
+  age_months += 12; //modular arithmetic to handle negative months
+  age_years--;
+}
+
+if (!age_months) {
+  document.getElementById(
+    "age"
+  ).innerHTML = `${age_years} years and ${age_days} days`;
+} else {
+  document.getElementById(
+    "age"
+  ).innerHTML = `${age_years} years, ${age_months} months and ${age_days} days`;
+}
